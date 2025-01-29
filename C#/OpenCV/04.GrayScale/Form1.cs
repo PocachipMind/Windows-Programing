871ccef3d34f58f480ea747da62519ccd2796918
이성규 <101550112+PocachipMind@users.noreply.github.com>
2025-01-29 18:27:01 +0900

    Add files via upload

---
+++ C#/OpenCV/04.GrayScale/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace OpenCV_Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        IplImage src; // IplImage형식으로 프레임을 불러와 이미지 형식으로 저장합니다.
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            src = new IplImage("../../Italia.jpg");
+            
+            // 클래스이름, 지역변수 이름, 생성자, 클래스 이름
+            OpenCV_Class Convert = new OpenCV_Class();
+
+            pictureBoxIpl1.ImageIpl = src;
+            pictureBoxIpl2.ImageIpl = Convert.GrayScare(src);
+            // Convert.GrayScare 메소드에서 사용할 매개변수를 Ipl이미지 불러오도록 하였으므로 그대로 원본값인 src를 인수로 사용합니다.
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {            
+            Cv.ReleaseImage(src); // 헤더 데이터 해제
+            if (src != null) { src.Dispose(); } // src데이터 해제
+        }
+
+        private void pictureBoxIpl2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ C#/OpenCV/04.GrayScale/OpenCV_Class.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace OpenCV_Practice
+{
+    internal class OpenCV_Class : IDisposable // 영상처리에서는 메모리관리가 중요하므로 해당 상속으로 관리되지않은 메모리를 해제할 수 있습니다.
+    {
+        IplImage gray;
+
+        // 그레이 스케일 메소드
+        public IplImage GrayScale(IplImage src)
+        {
+            // 그레이라는 필드는 어떤 값을 지니는지 생성하지 않았으므로 생성자를 이용하여 설정 
+            // 중요한 의미를 가지는 구문 ! 중요
+            gray = new IplImage(src.Size, BitDepth.U8, 1);
+
+            // cvt 컬러 메소드를 불러와 흑백 이미지로 변경
+            // 매개변수의 순서는 원본, 결과, 변환타입. 원본에서 입력된 이미지를 가지고 계산을 진행하게 됩니다.
+            // 이후 결과 이미지에 계산 결과를 저장하게 됩니다.
+            Cv.CvtColor(src, gray, ColorConversion.BgrToGray);
+            // 변환 타입은 어떻게 변환할지를 의미합니다. 그레이스케일로 변환할 예정이므로 블루,그린,레드에서 그레이로 변경합니다.
+
+            //결과로 저장된 그레이 필드를 반환
+            return gray;
+        }
+
+        public void Dispose()// 사용이 끝난 필드의 메모리 해제 구문을 추가
+        {
+            // 이미지를 흑백 색상으로 바꾸는 Grayscale메소드를 생성
+            // 만약 gray라는 필드가 비어있는 값이 아니라면 이미지의 메모리를 해제한다
+            if(gray != null) Cv.ReleaseImage(gray);
+        }
+    }
+}
